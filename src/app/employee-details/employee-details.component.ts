8c2d29a2969de5fd3464cd332c010f1f96a6554b
Akash Kumar Mehta <akmehta064@gmail.com>
2021-01-27 02:36:26 +0530

    import interface & used an interface as data type

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,4 +1,6 @@
+import { IEmployee } from './../models/deptinfo.interface';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-employee-details',
@@ -6,6 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-details.component.scss']
 })
 export class EmployeeDetailsComponent implements OnInit {
+  employees: IEmployee[]= [
+    {
+      name: "Employee One",
+      age: 40,
+      email: "one@gmail.com",
+      departments: ["Computer", "Physics"],
+    },
+    {
+      name: "Employee Two",
+      age: 10,
+      email: "Two@gmail.com",
+      departments: ["Computer"],
+    },
+    {
+      name: "Employee Three",
+      age: 10,
+      email: "Three@gmail.com",
+      departments: ["Physics", "Chemistry"],
+    },
+    {
+      name: "Employee Four",
+      age: 60,
+      email: "Four@gmail.com",
+      departments: ["Chemistry", "Physics"],
+    },
+    {
+      name: "Employee Five",
+      age: 70,
+      email: "Five@gmail.com",
+      departments: ["Computer", "Physics", "Chemistry"],
+    },
+    {
+      name: "Employee Six",
+      age: 70,
+      email: "Six@gmail.com",
+      departments: ["Computer", "Physics"],
+    }
+  ];
 
   constructor() { }
 
